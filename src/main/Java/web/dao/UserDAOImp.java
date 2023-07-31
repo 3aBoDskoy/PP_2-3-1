cd9827053192edf01dbf1fab4e299f085566e3ad
Pavel Zimogliad <3abodskoy123@gmail.com>
2023-07-31 17:41:59 +0300

    Initial

---
+++ src/main/Java/web/dao/UserDAOImp.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImp implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        String selectAllUser = "SELECT u FROM User u";
+        return entityManager.createQuery(selectAllUser, User.class).getResultList();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        entityManager.createQuery("delete from User u where u.id=:id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    @Override
+    public void editeUser(User user) {
+        entityManager.merge(user);
+    }
+}
---
+++ src/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>
+    <a href="/users">Users</a>
+</p>
+<p>
+    <a href="/update-user">User update</a>
+</p>
+<p>
+    <a href="/create-user">User create</a>
+</p>
+</body>
+</html>
